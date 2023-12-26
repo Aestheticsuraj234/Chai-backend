77e51e6d09147d675b5ccaf7b65286061ff415f6
Aestheticsuraj234 <jhapriya458@gmail.com>
2023-12-26 16:28:44 +0530

    Fix assignment bug in ApiError class

--- my_server_1/src/utils/ApiError.js
+++ my_server_1/src/utils/ApiError.js
@@ -10,7 +10,7 @@ class ApiError extends Error {
         this.data = null;
         this.message = message;
         this.success = false;
-        this.errors = this.errors;
+        this.errors = errors;
 
         if (stack) {
             this.stack = stack;
---
+++ my_server_1/src/utils/ApiResponse.js
@@ -0,0 +1,9 @@
+class ApiResponse {
+ constructor(statusCode , data , message = "Success", success = true)
+ {
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = statusCode <  400 ? true : false;
+ }
+}
